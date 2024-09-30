0fe94d21f2ed01b5d019261dd5e452ef733ac81e
Mykhailo Lytvynets <105230629+Liettualainen@users.noreply.github.com>
2024-09-30 14:37:37 +0300

    stylesFinixh

--- src/components/ContactBook/ContactBook.js
+++ src/components/ContactBook/ContactBook.js
@@ -2,7 +2,10 @@ import { Component } from "react";
 import { nanoid } from "nanoid";
 import {
   DivLayout, Phonebookstyle, FormFeedback, FormLebel, FormSection,
-  FormCheck, Button, InputName, FormRadio, FormSelect} from "./ContactBook.styled";
+  FormCheck, Button, InputName, FormRadio, FormSelect, ContactsDiv,
+  FindContactsDiv, FindContactsSpan, FindContactsInput, FindContactsUl, DeleteButton,
+  ContactsLi, ContactsSpanDiv,
+} from "./ContactBook.styled";
 // import { FeedbackOptions } from './FeedbackOptions.js';
 // import { Section } from './Section.js'
 const Gender = {
@@ -35,7 +38,6 @@ export class Contactbook extends Component {
   handleSubmit = evt => {
     evt.preventDefault();
     const { name, number, gender, age, agreed, contacts } = this.state;
-    console.log("state", this.state);
     const id = nanoid();
     if (agreed) {
       const contactsName = contacts.map(item => { item = item.name; return item })
@@ -53,55 +55,42 @@ export class Contactbook extends Component {
         alert("Please confirm correct info");
     }   
   }
-
   reset = () => {
     this.setState({ name: "", number: "", gender: "", age: "", agreed: "" })
     // this.setState({ ...INITIAL_STATE });
   };
-
   filterContact = evt => {
-    const { contacts, filter } = this.state;
     this.setState({ filter: evt.target.value });
-     console.log(contacts.filter(contact=>contact.name.toLowerCase().includes(filter.toLowerCase()) ))
   }
-
-  // deleteContact = id => {
-  //   console.log(id);
-  // }
-
-
   deleteContact = contactId => {
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== contactId)
     }));
   };
 
-
   render() {
-
    const {name, number, gender, age, agreed, contacts, filter } = this.state;
       return (
         <DivLayout>
-                 <Phonebookstyle>Phonebook
+      <Phonebookstyle>Phonebook
        </Phonebookstyle>
         <FormFeedback name="form" onSubmit={this.handleSubmit}>
         <FormLebel><InputName>Name</InputName>
-              <input type="text"
-                placeholder="Enter the name"
-              name="name" required
-             value={name}
+            <input type="text"
+            placeholder="Enter the name"
+            name="name" required
+            value={name}
             onChange={this.handleChange}
           />
             </FormLebel>
             <FormLebel><InputName>Number</InputName>
-              <input type="tel"
-                placeholder="Enter tel number"
-               name="number" required 
+            <input type="tel"
+            placeholder="Enter tel number"
+            name="number" required 
             value={number}
             onChange={this.handleChange}
           />
             </FormLebel>
-
         <FormSection>
               <InputName>Choose gender</InputName>
               <FormRadio>
@@ -127,7 +116,6 @@ export class Contactbook extends Component {
           </label>
               </FormRadio>
             </FormSection>
-
             <FormLebel>
               <InputName>Choose age</InputName>
               <FormRadio>
@@ -138,9 +126,7 @@ export class Contactbook extends Component {
             <option value="36+">36+</option>
           </FormSelect>
               </FormRadio>
-    
             </FormLebel>
-
         <FormCheck>
           <InputName>Confirm correct info </InputName>
               <input
@@ -150,48 +136,51 @@ export class Contactbook extends Component {
             onChange={this.handleChange}
           />
             </FormCheck>
-            
             <Button type="submit" >Add contact</Button>
-            
         </FormFeedback>
-          
-          <div>
+          <ContactsDiv>
             {(contacts.length > 0) &&
               <>
-                         <Phonebookstyle>Contacts
+              <Phonebookstyle>Contacts
               </Phonebookstyle>
-              <div style={{ display: "flex", flexDirection: "column", alignItems: "start", margin: 0, padding: 0, }}>
-                <span>Find contacts by name</span>
-               <input 
+              <FindContactsDiv>
+                <FindContactsSpan>Find contacts by name</FindContactsSpan>
+               <FindContactsInput 
             type="text"
             placeholder="Search contact"
             name="filter"
             //  value={filter}
             onChange={this.filterContact}
             />
-              </div>
+              </FindContactsDiv>
               {
-                (filter) ?
-                  <ul style={{ margin: 0, padding: 0, }}>  
-                           {contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase())).map(contact => (
-            <li key={contact.id} style={{ display: "flex", flexDirection: "row", alignItems: "start", margin: 0, padding: 0,}}>
-                <span>Name: {contact.name}:Number: {contact.number} Gender:{contact.gender} Age:{contact.age}</span>
-              <button onClick={this.deleteContact}>DELETE</button>
-            </li>
+                (filter) 
+                 ? <FindContactsUl style={{ margin: 0, padding: 0, }}>  
+            {contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase())).map(({id, name, number, gender, age}) => (
+            <ContactsLi key={id} style={{ display: "flex", flexDirection: "row", alignItems: "start", margin: 0, padding: 0,}}>
+                    <ContactsSpanDiv>
+                  <span>{name} tel.No:{number}</span>
+                <span>Gender: {gender}, Age: {age}</span>
+                </ContactsSpanDiv>
+              <DeleteButton onClick={()=> this.deleteContact(id)}>DELETE</DeleteButton>
+            </ContactsLi>
                            ))}
-                      </ul>
-                  : <ul style={{ margin: 0, padding: 0, }}>  
+              </FindContactsUl>
+                  : <FindContactsUl style={{ margin: 0, padding: 0, }}>  
             {contacts.map(({id, name, number, gender, age}) => (
-            <li key={id} style={{ display: "flex", flexDirection: "row", alignItems: "start", margin: 0, padding: 0,}}>
-                <span>Name: {name}:Number: {number} Gender:{gender} Age:{age}</span>
-              <button onClick={()=> this.deleteContact(id)}>DELETE</button>
-            </li>
+            <ContactsLi key={id} style={{ display: "flex", flexDirection: "row", alignItems: "start", margin: 0, padding: 0,}}>
+                <ContactsSpanDiv>
+                  <span>{name} tel.No:{number}</span>
+                <span>Gender: {gender}, Age: {age}</span>
+                </ContactsSpanDiv>
+              <DeleteButton onClick={()=> this.deleteContact(id)}>DELETE</DeleteButton>
+            </ContactsLi>
                     ))}
-          </ul>                
+          </FindContactsUl>                
 }
               </>
               } 
-          </div>
+          </ContactsDiv>
     </DivLayout>)
   }
 }
--- src/components/ContactBook/ContactBook.styled.js
+++ src/components/ContactBook/ContactBook.styled.js
@@ -8,14 +8,15 @@ export const Phonebookstyle = styled.span`
 margin-right: auto;
 font-size: 50px;
 font-weight: 700;
-margin-bottom: 10px;
+margin-bottom: 30px;
 `
 export const FormFeedback = styled.form`
 display: flex;
 flex-direction: column;
 align-items: start;
 border: 2px solid black;
-width: 500px;
+width: 650px;
+padding: 30px 80px;
 background-color: white;
 `
 export const FormLebel = styled.label`
@@ -93,6 +94,7 @@ border: 1px solid black;
   -webkit-user-select: none;
   background-color: white;
    color: green;
+   margin-left: auto;
 `
 export const StatisticsDiv = styled.div`
 display: flex;
@@ -111,4 +113,73 @@ export const StatisticsElem = styled.span`
 font-weight: 400;
 font-size: 50px;
 margin-top: 20px;
+`
+
+export const ContactsDiv = styled.div`
+display: flex;
+flex-direction: column;
+align-items: start;
+margin: 30px 0 0 0;
+`
+export const FindContactsDiv = styled.div`
+display: flex;
+flex-direction: column;
+align-items: start;
+margin: 20px 0 20px 0;
+`
+export const FindContactsSpan = styled.span`
+font-size: 25px;
+margin: 0px 0 10px 0;
+`
+export const FindContactsInput = styled.input`
+font-size: 20px;
+margin: 0px 0 10px 0;
+`
+
+export const FindContactsUl = styled.ul`
+display: flex;
+flex-direction: column;
+align-items: start;
+margin: 20px 0 20px 0;
+font-size: 25px;
+`
+
+export const DeleteButton = styled.button`
+  border-radius: 8px;
+border: 1px solid black;
+  box-sizing: border-box;
+  cursor: pointer;
+  display: inline-block;
+  font-family: "Haas Grot Text R Web", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 20px;
+  font-weight: 700;
+  width: 150px;
+  height: 40px;
+  line-height: 20px;
+  list-style: none;
+  margin: 10px;
+  outline: none;
+  padding: 10px 16px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  transition: color 100ms;
+  vertical-align: baseline;
+  user-select: none;
+  -webkit-user-select: none;
+  background-color: white;
+   color: red;
+`
+export const ContactsLi = styled.li`
+display: flex;
+flex-direction: row;
+width: 650px;
+`
+
+export const ContactsSpanDiv = styled.div`
+display: flex;
+flex-direction: column;
+align-items: start;
+width: 700px;
+
 `
